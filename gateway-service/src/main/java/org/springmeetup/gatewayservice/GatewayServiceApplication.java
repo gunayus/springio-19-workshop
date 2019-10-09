46c54d9d15f3c4287b98a8b010b4e3906ccc607d
TURKCELL\TCEGUNAY <erdem.gunay@turkcell.com.tr>
2019-10-09 13:12:07 -0500

    09_complete branch is finalized

--- gateway-service/src/main/java/org/springmeetup/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/org/springmeetup/gatewayservice/GatewayServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {

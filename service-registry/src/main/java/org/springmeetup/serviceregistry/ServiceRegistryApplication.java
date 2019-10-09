53635a26501ea7c928d43d39bc29ccd2abb81438
TURKCELL\TCEGUNAY <erdem.gunay@turkcell.com.tr>
2019-10-09 12:41:22 -0500

    08_gateway_service documentation is started

--- service-registry/src/main/java/org/springmeetup/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/org/springmeetup/serviceregistry/ServiceRegistryApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
